e170738c030205e8df2a167c6c674218ac9dc3b7
aleksey <kuzmenko_aleshka@bk.ru>
2018-04-24 02:30:02 +0300

    Fix bug with notification on Android devices with Oreo and higher

--- app/src/main/java/com/a_team/studentlife/NoticeManager/NoticeManager.java
+++ app/src/main/java/com/a_team/studentlife/NoticeManager/NoticeManager.java
@@ -1,34 +1,45 @@
 package com.a_team.studentlife.NoticeManager;
 
-import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 import com.a_team.studentlife.R;
 
-@SuppressLint("NewApi")
 public class NoticeManager {
     public static void showNotification(Context context,
                                         String ticker,
                                         String title,
                                         String text,
                                         int notificationId) {
-        Notification.Builder builder = new Notification.Builder(context);
-        builder
-                .setSmallIcon(R.mipmap.ic_launcher)
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notify_001")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
                 .setTicker(ticker)
                 .setContentTitle(title)
                 .setContentText(text)
                 .setWhen(System.currentTimeMillis())
-                .setAutoCancel(true);
-        Notification notification = builder.build();
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
         NotificationManager nm = (NotificationManager) context.getApplicationContext()
                                                               .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notify_001",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            if (nm != null) {
+                nm.createNotificationChannel(channel);
+            }
+        }
+
         if (nm != null) {
-            nm.notify(notificationId, notification);
+            nm.notify(notificationId, builder.build());
         }
     }
 }
