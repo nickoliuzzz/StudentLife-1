0762c163dfd6908369a199d69e79a48d385ed89d
aleksey <kuzmenko_aleshka@bk.ru>
2018-05-11 22:54:23 +0300

    Update createEvent design

--- app/src/main/java/com/a_team/studentlife/CreateEventActivity.java
+++ app/src/main/java/com/a_team/studentlife/CreateEventActivity.java
@@ -9,5 +9,6 @@ public class CreateEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event_activity);
+        setTitle("Создание события");
     }
 }
